5b5d8afc30e3de1afcae910c7c23e5e427e510f1
Rohit.Rasam <Rohit.Rasam@Neudesic.com>
2021-09-20 14:56:56 +0530

    Impemented Register method and GetInstance method

---
+++ LinkedinApp/LinkedinApp/LinkedinApp/Services/IActivityFeedService.cs
@@ -0,0 +1,13 @@
+﻿using LinkedinApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinApp.Services
+{
+    public interface IActivityFeedService
+    {
+        //Task<Activity> GetAllActivites();
+    }
+}

343c5259a958ec34dcf6ef8076eda142895bbfbb
saimanideep <saimanideep.chava@Neudesic.com>
2021-09-20 10:46:16 +0530

    Created a structure for linkedin Application

---
+++ LinkedinApp/LinkedinApp/LinkedinApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LinkedinApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace LinkedinApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ LinkedinApp/LinkedinApp/LinkedinApp/Views/MyNetwork.xaml.cs
@@ -0,0 +1,32 @@
+﻿using LinkedinApp.Models;
+using LinkedinApp.ViewModels;
+using LinkedinApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LinkedinApp.Views
+{
+    public partial class MyNetworkPage : ContentPage
+    {
+        MyNetworkViewModel _viewModel;
+
+        public MyNetworkPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new MyNetworkViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
